21da9d33ee324331c998d9e9c5569640e82840a6
lagrange2art <tobias.becker@tu-berlin.de>
2022-07-06 22:49:09 +0200

    feature to adjust brightness of ledstrip on the fly

--- app.py
+++ app.py
@@ -2,17 +2,19 @@
 this is run at startup. To change this behavior delete call from /etc/rc.local
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from ledstrip.fillstrip import lightup
 #from PiCam.ambilight import Ambilight
 
 import threading  # create independent thread for ambilight
 
+import numpy as np
  
 #amblight = Ambilight()
 
 app = Flask(__name__)
 
+brightness = 0.5   # initial brightness
 
 @app.route('/')
 def index():
@@ -36,8 +38,26 @@ def fillstrip(rgb):
     according to rgb values. 
     param str rgb: string with comma seperated rgb values """
     #amblight._FINISH = True  # stop ambilight if running        
+
+    # adjust brightness of rgb = 'r,g,b'
+    rgb = np.array(rgb.split(',')).astype(int)   # rgb = [r,g,b]
+    rgb = (brightness*rgb).astype(int)
+    rgb = ','.join(rgb.astype(str))              # rgb = 'r,g,b'
     lightup(rgb)
     return render_template('fillstrip.html', name=rgb)
+
+@app.route('/fillstrip/<rgb>', methods=['POST', 'GET'])
+def set_brightness(rgb):
+    """ use interface on webapp to change global variable that changes brightness"""
+    global brightness
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        # set new brightness
+        brightness = float(request.form['Brightness'])
+        # show new brightness if 'update' button is pressed
+        fillstrip(rgb)
+        return render_template('fillstrip.html', name=rgb)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', threaded=True)
